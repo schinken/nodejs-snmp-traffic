2b10addf7d49a27881c3d6b962555c850747fad9
schinken <schinken@hackerspace-bamberg.de>
2012-09-25 22:55:16 +0200

    removed name from anonymous function

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ module.exports = {
     'map_range': function( val, min1, max1, min2, max2 ) {
         return min2 + (max2 - min2) * ((val - min1) / (max1 - min1));
     },
-    'clamp': function clamp( val, max, min ) {
+    'clamp': function( val, max, min ) {
         if( val > max ) {
             return max;
         }    
